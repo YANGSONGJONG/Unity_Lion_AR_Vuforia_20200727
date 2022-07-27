b9815b5aa71ec51231d8c789bacb2cbc4425dad6
YANGSONGJONG <sx224367543@gmail.com>
2022-07-27 16:30:23 +0800

    Update 完成

--- Unity VR/Assets/Scripts/CardManager.cs
+++ Unity VR/Assets/Scripts/CardManager.cs
@@ -1,5 +1,7 @@
 
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 
 namespace KID.AR.Vuforia
 {
@@ -10,11 +12,30 @@ namespace KID.AR.Vuforia
     {
         [SerializeField, Header("KID 圖片")]
         private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("小騎士")]
+        private Animator aniKnight;
+        [SerializeField, Header("攻擊按鈕")]
+        private Button btnAttack;
+        [SerializeField, Header("虛擬按鈕跳躍")]
+        private VirtualButtonBehaviour vbbJump;
+
+        private string parVictory = "觸發勝利";
+        private AudioSource audBGM;
 
         private void Awake()
         {
             observerKID.OnTargetFound.AddListener(CardFound);
             observerKID.OnTargetLost.AddListener(CardLost);
+            btnAttack.onClick.AddListener(Attack);
+
+            vbbJump.RegisterOnButtonPressed(OnJumpPressed);
+
+            audBGM = GameObject.Find("BGM").GetComponent<AudioSource>();
+        }
+
+        private void OnJumpPressed(VirtualButtonBehaviour obj)
+        {
+            print("<color=#3366dd>跳躍!!!</color>");
         }
 
         /// <summary>
@@ -23,6 +44,8 @@ namespace KID.AR.Vuforia
         private void CardFound()
         {
             print("<color=yellow>找到卡片</color>");
+            aniKnight.SetTrigger(parVictory);
+            audBGM.Play();
         }
         /// <summary>
         /// 圖片辨識失敗
@@ -30,6 +53,12 @@ namespace KID.AR.Vuforia
         private void CardLost()
         {
             print("<color=red>卡片辨識失敗</color>");
+            audBGM.Stop();
+        }
+
+        private void Attack()
+        {
+            print("<color=#9955aa>攻擊!!!</color>");
         }
     }
 
