062af2bfc4c6009856721dacfc380451d6d1cc93
YANGSONGJONG <sx224367543@gmail.com>
2022-08-03 11:27:30 +0800

    Update 平面辨識完成

--- Unity VR/Assets/Scripts/CardManager.cs
+++ Unity VR/Assets/Scripts/CardManager.cs
@@ -20,6 +20,8 @@ namespace KID.AR.Vuforia
         private VirtualButtonBehaviour vbbJump;
 
         private string parVictory = "Ä²µo³Ó§Q";
+        private string parJump = "Ä²µo¸õÅD";
+        private string parAttack = "Ä²µo§ðÀ»";
         private AudioSource audBGM;
 
         private void Awake()
@@ -36,6 +38,7 @@ namespace KID.AR.Vuforia
         private void OnJumpPressed(VirtualButtonBehaviour obj)
         {
             print("<color=#3366dd>¸õÅD!!!</color>");
+            aniKnight.SetTrigger(parJump);
         }
 
         /// <summary>
@@ -59,6 +62,7 @@ namespace KID.AR.Vuforia
         private void Attack()
         {
             print("<color=#9955aa>§ðÀ»!!!</color>");
+            aniKnight.SetTrigger(parAttack);
         }
     }
 
